a3cdbeb1d1f606e8dfd2e316d362453af45e8c1e
xwjxwj7430 <xuwenjun7430@163.com>
2019-01-25 16:46:23 +0800

    20190125 work push

--- Day 10 20190125/List_practice2.py
+++ Day 10 20190125/List_practice2.py
@@ -2,8 +2,6 @@ products = [['iphone8', 6888], ['MacPro', 14800], ['小米6', 2499], ['Coffee', 
 
 print('------------商品列表-----------')
 
-count = 0
-for i in products:
-    print('%s. %s  %s' % (count, i[0], i[1]))
-    count += 1
+for index, i in enumerate(products):
+    print('%s. %s  %s' % (index, i[0], i[1]))
 
