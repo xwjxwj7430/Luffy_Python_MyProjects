ffeea028173049337e4cc5b865189904d9c57dd4
xwjxwj7430 <xuwenjun7430@163.com>
2019-01-25 16:57:47 +0800

    20190125 work push

--- Day 10 20190125/List_practice3.py
+++ Day 10 20190125/List_practice3.py
@@ -14,9 +14,9 @@ while True:
     add_to_cart = input('请问您想买几号商品：')
     if add_to_cart.isdigit():
         cart.append(products[int(add_to_cart)])
+        print('您目前购物车内的商品为 ', cart)
     elif add_to_cart == 'q':
         break
-    print('您目前购物车内的商品为 ', cart)
 
 print('------------结账清单-----------')
 
